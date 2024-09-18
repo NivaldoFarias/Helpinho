17834622be676d9a6f6e2fe6ef0ac44a9921a62c
NivaldoFarias <badivia.f@gmail.com>
2024-09-18 10:16:32 -0300

    chore[front]: clean up project directories structure

--- frontend/src/app/app.component.ts
+++ frontend/src/app/app.component.ts
@@ -3,11 +3,12 @@ import { RouterOutlet } from "@angular/router";
 
 import { LogotypeComponent } from "./components/logotype/logotype.component";
 import { TextureComponent } from "./components/texture/texture.component";
+import { HomepageComponent } from "./pages/home/home.component";
 
 @Component({
 	selector: "app-root",
 	standalone: true,
-	imports: [RouterOutlet, TextureComponent, LogotypeComponent],
+	imports: [RouterOutlet, TextureComponent, LogotypeComponent, HomepageComponent],
 	templateUrl: "./app.component.html",
 })
 export class AppComponent {
---
+++ frontend/src/app/pages/home/home.component.ts
@@ -0,0 +1,8 @@
+import { Component } from "@angular/core";
+
+@Component({
+	standalone: true,
+	selector: "app-homepage",
+	templateUrl: "./home.component.html",
+})
+export class HomepageComponent {}
